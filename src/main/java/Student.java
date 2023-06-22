f7fc87904c1c1a036803a34f130bc2376f4ce65c
mark-gallagher <mtgallagher21@gmail.com>
2023-06-22 18:52:52 +0100

    Added file.txt; Updated fields, getters & setters, constructor, toString and StudentList

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,12 +1,33 @@
+import java.time.LocalDate;
+
 public class Student {
 
+    private int studentId;
     private String name;
     private String email;
+    private LocalDate dob;
+    private String location;
 
-    public Student(String name, String email) {
-        setName(name);
-        setEmail(email);
+    public Student(int studentId, String name, String email, LocalDate dob, String location) {
+        this.studentId = studentId;
+        this.name = name;
+        this.email = email;
+        this.dob = dob;
+        this.location = location;
+    }
 
+    public Student(int studentId, String name, String email) {
+        this.studentId = studentId;
+        this.name = name;
+        this.email = email;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
     }
 
     public String getName() {
@@ -23,5 +44,32 @@ public class Student {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "studentId=" + studentId +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", dob=" + dob +
+                ", location='" + location + '\'' +
+                '}';
     }
 }
--- src/main/java/StudentList.java
+++ src/main/java/StudentList.java
@@ -4,11 +4,11 @@ public class StudentList {
     public static void main(String[] args) {
         ArrayList<String> studentName = new ArrayList<>();
         studentName.add(1,"Chris Barry");
-        studentName.add(2,"Murray Scott");
+        studentName.add(2,"Joe O'Donnell");
 
         ArrayList<String> studentEmail = new ArrayList<>();
-        studentEmail.add(3,"cbarry@gmit.ie");
-        studentEmail.add(4,"mscott@gmit.ie");
+        studentEmail.add(3,"cbarry@atu.ie");
+        studentEmail.add(4,"jOdonnell@atu.ie");
 
 
     }
